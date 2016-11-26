fff26de0c4a0e302201d58f268d1eda750bf0475
arturlicks <arturlicks@gmail.com>
2016-11-26 18:47:40 -0200

    Create binaries.cpp

---
+++ binaries.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector <int> bin;
+
+void tobin(int n)
+    {
+    if (n > 0)
+        {
+        bin.push_back(n % 2);
+        tobin(n / 2);
+    }
+}
+
+int main(){
+    int n, count = 0, aux = 0;
+    cin >> n;
+
+    tobin(n);
+
+    for (int i=bin.size() - 1; i >= 0; i-- )
+        {
+//            if (bin[i] == 1  && count == 0)
+//                count++;
+            if ((bin[i] == bin[i-1] || count == 0) && bin[i] == 1)
+                count++;
+            else if (count > aux)
+                {
+                    aux = count;
+                    count = 0;
+                }
+
+        }
+        //cout << bin[i];
+        cout << aux;
+
+
+
+    return 0;
+}
